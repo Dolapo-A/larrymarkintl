5ecd055cf68e509bd538919fa58fb0f63f3fb490
Dolapo-crypto <araoyeemmanuel2001@gmail.com>
2023-12-06 02:41:59 +0100

    **

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,28 @@
 // webpack.config.js
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  mode: 'development', // or 'production'
-  entry: './src/index.js', // Adjust this based on your project structure
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'), // Output directory, adjust as needed
-  },
+	mode: "development", // or 'production'
+	entry: "./src/index.js", // Adjust this based on your project structure
+	output: {
+		filename: "bundle.js",
+		path: path.resolve(__dirname, "dist"), // Output directory, adjust as needed
+	},
+};
+
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+	entry: "./src/index.js",
+	output: {
+		filename: "bundle.js",
+		path: path.resolve(__dirname, "dist"),
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: "./src/index.html",
+			filename: "index.html",
+		}),
+	],
 };
