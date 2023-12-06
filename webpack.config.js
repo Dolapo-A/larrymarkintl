068a10242a13641707b21ade6e4e2dc8ed573a92
Dolapo-crypto <araoyeemmanuel2001@gmail.com>
2023-12-06 02:04:35 +0100

    index.js | webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+// webpack.config.js
+const path = require('path');
+
+module.exports = {
+  mode: 'development', // or 'production'
+  entry: './src/index.js', // Adjust this based on your project structure
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'), // Output directory, adjust as needed
+  },
+};
